db81f8e68729d59ada2be5b4aea937c48b433dac
subterraneanbob <mp710@yandex.ru>
2014-03-31 13:55:01 +0300

    Add current date support as default value of a column.

--- src/FluentMigrator.Runner/Generators/Oracle/OracleColumn.cs
+++ src/FluentMigrator.Runner/Generators/Oracle/OracleColumn.cs
@@ -62,6 +62,8 @@ namespace FluentMigrator.Runner.Generators.Oracle
             {
                 case SystemMethods.NewGuid:
                     return "sys_guid()";
+                case SystemMethods.CurrentDateTime:
+                    return "sysdate";
             }
 
             return null;
